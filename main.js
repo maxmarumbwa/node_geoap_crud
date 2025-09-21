c3d8f573f5af5b2bf55619860aade303efdceaf3
maxmarumbwa <maxmarumbwa@gmail.com>
2025-09-21 08:26:03 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,42 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const session = require('express-session');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+//Database connection
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', (error) => console.log(error));
+db.once('open', () => console.log('Connected to the database!'));
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+//Middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(session({
+    secret: "my secret key",
+    saveUninitialized: true,
+    resave: false,
+}));
+
+app.use((req, res, next) => {
+    res.locals.message = req.session.message;
+    delete req.session.message;
+    next();
+});
+
+// set engine template
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server started at http://localhost:${PORT}`);
+});
---
+++ models/users.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    image: {
+        type: String,
+        required: true,
+    },
+    created: {
+        type: Date,
+        required: true,
+    },
+        // Additional fields can be added here
+    });
